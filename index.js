c0628942452f5687aa197010446939060829ecfb
Yumin-Kim <dbals0@naver.com>
2020-12-22 19:21:25 +0900

    Connection Mysql and Heroku

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const app = require("express")();
+const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
 const PORT = process.env.PORT ? process.env.PORT : 3000;
 
-app.get("/",(req,res)=>{
-    res.send("Hello");
-    res.end();
-})
+const app = express();
 
+dotenv.config();
+app.use("/", express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    database:process.env.DB_DATABASE,
+    user : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD
+})
+app.get("/",  (req,res)=>{
+    pool.getConnection((error , conn)=>{
+        if(error) console.error(error);
+        const str = "select * from user"; 
+        conn.query(str,(error,result)=>{
+            if(error) throw error;
+            console.log(result)
+            res.status(200).send(`<h1>${result[0].id}${result[0].age}${result[0].name}${result[0].description}고객</h1>`)
+            //res.status(200).send(`<h1>${result[0].id}${result[0].age}${result[0].name}${result[0].description}고객</h1>`)
+            conn.release();
+        })
+    })
+})
 app.listen(PORT,()=>{
+    
     console.log("Start");
 })
