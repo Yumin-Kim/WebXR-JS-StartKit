0c1b27dcb0e2b9e7b7c47526a24d0341042f9e3f
Yumin-Kim <dbals0@naver.com>
2020-12-23 17:50:32 +0900

    update 360 photo Folder

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT ? process.env.PORT : 3000;
 const app = express();
 
 dotenv.config();
-app.use("/", express.static("public"));
+app.use("/", express.static("public/marzipano_2"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const pool = mysql.createPool({
@@ -16,19 +16,7 @@ const pool = mysql.createPool({
     password : process.env.DB_PASSWORD
 })
 app.get("/",  (req,res)=>{
-    // pool.getConnection((error , conn)=>{
-    //     if(error) console.error(error);
-    //     const str = "select * from user"; 
-    //     conn.query(str,(error,result)=>{
-    //         if(error) throw error;
-    //         console.log(result)
-    //         res.write(`<h1>${result[0].id}${result[0].age}${result[0].name}${result[0].description}고객</h1>`)
-    //         res.end()
-    //         //res.status(200).send(`<h1>${result[0].id}${result[0].age}${result[0].name}${result[0].description}고객</h1>`)
-    //         conn.release();
-    //     })
-    // })
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/public/marzipano_2/index.html");
 })
 app.listen(PORT,()=>{
     console.log("Start");
