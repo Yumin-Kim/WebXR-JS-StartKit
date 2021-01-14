d189c0707efd66f8db3e9bdc9e4b5ee61a1c1516
Yumin-Kim <dbals0@naver.com>
2021-01-14 23:40:37 +0900

    Update

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.use("/", express.static("public/aframe-blink-teleportation"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    database:process.env.DB_DATABASE,
-    user : process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORD
-})
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     database:process.env.DB_DATABASE,
+//     user : process.env.DB_USERNAME,
+//     password : process.env.DB_PASSWORD
+// })
 
 app.get("/",  (req,res)=>{
     res.sendFile(__dirname + "/public/aframe-blink-teleportation/index.html");
