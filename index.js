6d5fb698249c7a3cb8bb87e582c29bcbf439d3a1
Yumin-Kim <dbals0@naver.com>
2021-05-15 19:22:56 +0900

    connect Database

--- index.js
+++ index.js
@@ -11,19 +11,38 @@ app.use("/", express.static("public/examples"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     database:process.env.DB_DATABASE,
-//     user : process.env.DB_USERNAME,
-//     password : process.env.DB_PASSWORD
-// })
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+});
+conn.connect();
+const insertSQL =
+  "INSERT INTO `heroku_4f519a792970c53`.`user` (`user_id`, `name`, `passwd`, `access_code`, `dept`, `sub_dept`, `phone_number`) VALUES ('2', 'qwe', '123', '1', '123', '123', '123123')";
+app.post("/login", (req, res) => {
+  const data = req.body;
+  const parseData = Object.values(JSON.parse(data.json)).reduce(
+    (prev, cur, index) => {
+      prev.push(cur);
+      return prev;
+    },
+    []
+  );
+  conn.query(insertSQL, parseData, (error, row, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(row);
+      console.log(fields);
+      res.json({ operation: "sucess" });
+    }
+  });
+});
 
-// app.get("/",  (req,res)=>{
-//     res.sendFile(__dirname + "/public/webpackBuildAframe/index.html");
-// })
-app.get("/",  (req,res)=>{
-    res.sendFile(__dirname + "/public/examples/physics/index.html");
-})
-app.listen(PORT,()=>{
-    console.log("Start");
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/examples/physics/index.html");
+});
+app.listen(PORT, () => {
+  console.log("Start");
+});
