3398395640dd97a57e804c78cea0cee93253589b
Yumin-Kim <dbals0@naver.com>
2021-06-20 18:21:37 +0900

    Update html Route

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 dotenv.config();
 
-app.use("/", express.static("public/examples"));
+app.use("/", express.static("public/marzipano_5"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
